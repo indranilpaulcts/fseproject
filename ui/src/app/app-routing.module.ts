d817c2214ce2200ed7c38cd4179a848477db87ac
indranilpaulcts <indranil.paul@cognizant.com>
2019-06-26 15:26:10 +0530

    Initial UI commit

---
+++ ui/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
+
+const routes: Routes = [
+  {
+    path: 'addtask',
+    component: AddtaskComponent
+  },
+  {
+    path: 'viewtask',
+    component: ViewtaskComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddtaskComponent,
+    ViewtaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
